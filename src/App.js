5810dbbc760f52f6e6ae7ae3cf99d4de6bdab9d3
erfanhabeeb <erfanhabeeb30@gmail.com>
2025-10-12 04:19:10 +0530

    added text

--- src/App.js
+++ src/App.js
@@ -6,8 +6,12 @@ import Admin from "./pages/Admin";
 function App() {
   return (
     <BrowserRouter>
-      <nav className="bg-gray-800 text-white p-4 flex justify-between">
+      <nav className="bg-gray-800 text-white p-4 flex justify-between items-center">
         <Link to="/" className="font-bold">Resonance 2025</Link>
+        <div className="text-right leading-tight">
+          <div className="text-sm">Union Christian College (Autonomous), Aluva</div>
+          <div className="text-xs opacity-90">Union Christian Institute of Management and Technology or UCIMT</div>
+        </div>
         {/* <Link to="/admin" className="hover:underline">Admin</Link> */}
       </nav>
       <Routes>
